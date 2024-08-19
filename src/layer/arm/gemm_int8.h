7ef4a3c351f1fab5312ed0c2390bade3f06baa31
nihuini <nihuini@tencent.com>
2024-08-19 17:53:44 +0800

    w

--- src/layer/arm/gemm_int8.h
+++ src/layer/arm/gemm_int8.h
@@ -1899,16 +1899,24 @@ static void unpack_output_tile_int32_to_fp32(const Mat& topT, const Mat& C, Mat&
     const int out_elempack = top_blob.elempack;
     const int out_hstep = top_blob.dims == 3 ? (int)top_blob.cstep : top_blob.w;
 
+    // if (broadcast_type_C == 3)
     // NCNN_LOGE("unpack_output_tile_int32_to_fp32  %d %d %d %d  %d  %d", i, max_ii, j, max_jj, out_elempack, broadcast_type_C);
 
-    // if (broadcast_type_C != -1)
+    // if (broadcast_type_C == 3)
     // NCNN_LOGE("unpack_output_tile_int32_to_fp32 %d %d    %d    %d %d %d", out_elempack, C.elempack, broadcast_type_C, C.w, C.h, C.c);
 
     // TODO __aarch64__
     // TODO __ARM_FEATURE_DOTPROD
 
     const int c_hstep = C.dims == 3 ? (int)C.cstep * out_elempack : C.w * out_elempack;
+    // const int c_hstep = C.dims == 3 ? (int)C.cstep : C.w;
     const float* pC = C;
+
+    // if (broadcast_type_C == 3)
+    // NCNN_LOGE("C.dims %d   c_hstep  %d", C.dims, c_hstep);
+
+    // if (broadcast_type_C == 0)
+    // NCNN_LOGE("C %f", C[0]);
 
     const int* pp = topT;
 
@@ -5042,25 +5050,25 @@ static void transpose_unpack_output_tile_int32_to_fp32(const Mat& topT, Mat& top
             for (; jj + 7 < max_jj; jj += 8)
             {
                 // a0 a1 a2 a3
-                // b0 b1 b2 b3
                 // a4 a5 a6 a7
+                // b0 b1 b2 b3
                 // b4 b5 b6 b7
 
                 p0[0] = pp[0] * descale;
-                p0[1] = pp[4] * descale;
+                p0[1] = pp[8] * descale;
                 p0[out_hstep] = pp[1] * descale;
-                p0[out_hstep + 1] = pp[5] * descale;
+                p0[out_hstep + 1] = pp[9] * descale;
                 p0[out_hstep * 2] = pp[2] * descale;
-                p0[out_hstep * 2 + 1] = pp[6] * descale;
+                p0[out_hstep * 2 + 1] = pp[10] * descale;
                 p0[out_hstep * 3] = pp[3] * descale;
-                p0[out_hstep * 3 + 1] = pp[7] * descale;
-                p0[out_hstep * 4] = pp[8] * descale;
+                p0[out_hstep * 3 + 1] = pp[11] * descale;
+                p0[out_hstep * 4] = pp[4] * descale;
                 p0[out_hstep * 4 + 1] = pp[12] * descale;
-                p0[out_hstep * 5] = pp[9] * descale;
+                p0[out_hstep * 5] = pp[5] * descale;
                 p0[out_hstep * 5 + 1] = pp[13] * descale;
-                p0[out_hstep * 6] = pp[10] * descale;
+                p0[out_hstep * 6] = pp[6] * descale;
                 p0[out_hstep * 6 + 1] = pp[14] * descale;
-                p0[out_hstep * 7] = pp[11] * descale;
+                p0[out_hstep * 7] = pp[7] * descale;
                 p0[out_hstep * 7 + 1] = pp[15] * descale;
                 pp += 16;
                 p0 += out_hstep * 8;
@@ -6475,7 +6483,7 @@ static void unpack_output_tile_int32_to_bf16(const Mat& topT, const Mat& C, Mat&
     const int out_elempack = top_blob.elempack;
     const int out_hstep = top_blob.dims == 3 ? (int)top_blob.cstep : top_blob.w;
 
-    // NCNN_LOGE("unpack_output_tile_int32_to_fp32  %d %d %d %d  %d  %d", i, max_ii, j, max_jj, out_elempack, broadcast_type_C);
+    // NCNN_LOGE("unpack_output_tile_int32_to_bf16  %d %d %d %d  %d  %d", i, max_ii, j, max_jj, out_elempack, broadcast_type_C);
 
     // if (broadcast_type_C != -1)
     // NCNN_LOGE("unpack_output_tile_int32_to_fp32 %d %d    %d    %d %d %d", out_elempack, C.elempack, broadcast_type_C, C.w, C.h, C.c);
@@ -8939,7 +8947,7 @@ static void transpose_unpack_output_tile_int32_to_bf16(const Mat& topT, Mat& top
     const int out_elempack = top_blob.elempack;
     const int out_hstep = top_blob.dims == 3 ? (int)top_blob.cstep : top_blob.w;
 
-    // NCNN_LOGE("transpose_unpack_output_tile_int32_to_fp32 %d %d %d %d    %d", i, max_ii, j, max_jj, out_elempack);
+    // NCNN_LOGE("transpose_unpack_output_tile_int32_to_bf16 %d %d %d %d    %d", i, max_ii, j, max_jj, out_elempack);
 
     // TODO __ARM_FEATURE_DOTPROD
     // TODO broadcast_type_C
@@ -9809,25 +9817,25 @@ static void transpose_unpack_output_tile_int32_to_bf16(const Mat& topT, Mat& top
             for (; jj + 7 < max_jj; jj += 8)
             {
                 // a0 a1 a2 a3
-                // b0 b1 b2 b3
                 // a4 a5 a6 a7
+                // b0 b1 b2 b3
                 // b4 b5 b6 b7
 
                 p0[0] = float32_to_bfloat16(pp[0] * descale);
-                p0[1] = float32_to_bfloat16(pp[4] * descale);
+                p0[1] = float32_to_bfloat16(pp[8] * descale);
                 p0[out_hstep] = float32_to_bfloat16(pp[1] * descale);
-                p0[out_hstep + 1] = float32_to_bfloat16(pp[5] * descale);
+                p0[out_hstep + 1] = float32_to_bfloat16(pp[9] * descale);
                 p0[out_hstep * 2] = float32_to_bfloat16(pp[2] * descale);
-                p0[out_hstep * 2 + 1] = float32_to_bfloat16(pp[6] * descale);
+                p0[out_hstep * 2 + 1] = float32_to_bfloat16(pp[10] * descale);
                 p0[out_hstep * 3] = float32_to_bfloat16(pp[3] * descale);
-                p0[out_hstep * 3 + 1] = float32_to_bfloat16(pp[7] * descale);
-                p0[out_hstep * 4] = float32_to_bfloat16(pp[8] * descale);
+                p0[out_hstep * 3 + 1] = float32_to_bfloat16(pp[11] * descale);
+                p0[out_hstep * 4] = float32_to_bfloat16(pp[4] * descale);
                 p0[out_hstep * 4 + 1] = float32_to_bfloat16(pp[12] * descale);
-                p0[out_hstep * 5] = float32_to_bfloat16(pp[9] * descale);
+                p0[out_hstep * 5] = float32_to_bfloat16(pp[5] * descale);
                 p0[out_hstep * 5 + 1] = float32_to_bfloat16(pp[13] * descale);
-                p0[out_hstep * 6] = float32_to_bfloat16(pp[10] * descale);
+                p0[out_hstep * 6] = float32_to_bfloat16(pp[6] * descale);
                 p0[out_hstep * 6 + 1] = float32_to_bfloat16(pp[14] * descale);
-                p0[out_hstep * 7] = float32_to_bfloat16(pp[11] * descale);
+                p0[out_hstep * 7] = float32_to_bfloat16(pp[7] * descale);
                 p0[out_hstep * 7 + 1] = float32_to_bfloat16(pp[15] * descale);
                 pp += 16;
                 p0 += out_hstep * 8;
